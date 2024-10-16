86ffb7edd7504dab9f88271b31223f7ef0743e60
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-16 10:39:53 -0400

    finds distance to object using sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joos
+ * Created on: Oct 2024
+ * This program finds distance to object using sonar
 */
 
-basic.showString('Hello, World!')
+let objectDistance:number = 0
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+//getting distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    objectDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(objectDistance)
+    basic.showIcon(IconNames.Happy)
+})
